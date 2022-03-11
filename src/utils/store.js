e82345802eef94abe2ef9b4fa9dc517467323b8e
R.Charlot <romain.charlot@lilo.org>
2022-03-11 13:32:44 +0100

    replace store creation by @reduxjs/toolkit configureStore

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,20 +1,17 @@
 //Redux store
 
-import { createStore, combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 
 import { fetchLoginReducer } from '../features/fetchLogin'
 import { fetchPostProfileReducer } from '../features/fetchUser'
 import { fetchPutProfileReducer } from '../features/fetchUpdate'
 import { userReducer } from '../features/user'
 
-const reduxDevtools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
-const reducer = combineReducers({
-  fetchLogin: fetchLoginReducer,
-  fetchUser: fetchPostProfileReducer,
-  fetchUpdate: fetchPutProfileReducer,
-  user: userReducer,
+export default configureStore({
+  reducer: {
+    fetchLogin: fetchLoginReducer,
+    fetchUser: fetchPostProfileReducer,
+    fetchUpdate: fetchPutProfileReducer,
+    user: userReducer,
+  },
 })
-
-export const store = createStore(reducer, reduxDevtools)
